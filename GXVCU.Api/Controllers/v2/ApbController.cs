8732eaef592c39d02e75673df802d65e81ec1840
273494627 <273494627@qq.com>
2020-12-11 15:25:49 +0800

    添加打印接口

--- GXVCU.Api/Controllers/v2/ApbController.cs
+++ GXVCU.Api/Controllers/v2/ApbController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GXVCU.Api.Comm;
-using Microsoft.AspNetCore.Http;
+using GXVCU.Common;
+using GXVCU.Common.Helper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace GXVCU.Api.Controllers.v2
 {
@@ -12,11 +12,62 @@ namespace GXVCU.Api.Controllers.v2
     [ApiController]
     public class ApbController : ControllerBase
     {
+        private readonly ILogger<ApbController> _logger;
+
+        public ApbController(ILogger<ApbController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         [CustomRoute(ApiVersions.V2, "apbs")]
         public IEnumerable<string> Get()
         {
             return new string[] { "第二版的 apbs" };
         }
+
+        /// <summary>
+        /// 打印测试
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [CustomRoute(ApiVersions.V2, "Print")]
+        public MessageModel<string> Print()
+        {
+            var data = new MessageModel<string>();
+            try
+            {
+                var v = new EntityBitmapLHGQ
+                {
+                    PartName = "整车控制器",
+                    Hardware = "Hardware",
+                    Software = "Software",
+                    Company = "联合汽车电子有限公司",
+                    DateTime = "2018/12/21",
+                    VIN = "NO.0001",
+                    Sign = "S1",
+                    Num = "A18",
+                    CodeText = " ",
+                    PartNum = "零件号",
+                    SW = "SW",
+                    HW = "HW",
+                };
+                var print = new HelperPrintDocument();
+                print.Print(v);
+                data.Response = "已经发出打印请求！";
+                data.Success = true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                data.Success = false;
+                data.Response = "打印失败！";
+                data.Msg = ex.Message;
+            }
+            return data;
+        }
+
+
+        
     }
 }
---
+++ GXVCU.Common/Helper/HelperLog.cs
@@ -0,0 +1,22 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GXVCU.Common.Helper
+{
+    public class HelperLog
+    {
+        private static readonly ILog logger = LogManager.GetLogger("GXVCU.Common");
+
+        public static void Error(object message, Exception exception)
+        {
+            logger.Error(message, exception);
+        }
+
+        public static void Error(object message)
+        {
+            logger.Error(message);
+        }
+    }
+}
